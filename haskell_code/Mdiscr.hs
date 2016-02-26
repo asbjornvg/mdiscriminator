f653eb5cf15acf5c28261b8fa8babd0d25e5653b
Ásbjørn Viderø Jøkladal <asbjornvg@gmail.com>
2016-02-26 15:34:40 +0100

    Finished the segmented version in Haskell, and adjusted the non-segmented version a bit to keep it aligned with the segmented version.

--- haskell_code/Mdiscr.hs
+++ haskell_code/Mdiscr.hs
@@ -6,52 +6,49 @@ module Mdiscr
 import Helpers
 
 -- Generalized version with a single scan.
-mdiscr :: Int -> (a -> Int) -> [a] -> ([a], [Int])
-mdiscr m discr arr = (new_arr, sizes)
-    where
-      -- The length of the input array.
-      n = length arr
-      
-      -- Find the equivalence classes using the discriminator.
-      classes = map discr arr
-      
-      -- Here, we could assert that all the classes are between 0 and m-1.
-      
-      -- Create a list with [0,...,1,...,0] of length m where the 1 is in
-      -- the k'th position.
-      createColumn :: Int -> [Int]
-      createColumn k =
-          let
-              zeros = replicate m 0
-              (ys,_:zs) = splitAt k zeros
-          in
-            ys ++ (1 : zs)
-      
-      -- First turn the classes into columns (lists), then scan. No offsets are
-      -- added yet.
-      scan_result = (scanInc (zipWith (+)) (replicate m 0) .
-                     map createColumn) classes
-      
-      -- The last column contains the reductions for each class, i.e., the
-      -- total number of elements belonging to each class.
-      sizes = last scan_result
-      
-      -- Since all the elements of class k come before the elements of class
-      -- k+1, we must accumulate the sizes into offsets to add to each class.
-      offsets = scanExc (+) 0 sizes
-      
-      -- Add the offsets.
-      scan_result_with_offsets = map (zipWith (+) offsets) scan_result
-      
-      -- Now, the indices for elements of class k can be found at the k'th entry
-      -- in the corresponding column of the scan result, so we must extract the
-      -- appropriate entries from the columns. These indices are 1-indexed, so
-      -- we must remember to subtract 1.
-      indices = zipWith (\k column -> column !! k - 1)
-                classes scan_result_with_offsets
-      
-      new_arr = permute indices arr
-      
-      -- -- Write a flag array containing the sizes (i.e., the number of elements
-      -- -- belonging to each class) at the positions given by the offsets.
-      -- flags = write offsets sizes (replicate n 0)
+mdiscr :: Int -> (a -> Int) -> [a] -> ([Int], [a])
+mdiscr m discr arr =
+    let
+        -- The length of the input array.
+        n = length arr
+        
+        -- Find the equivalence classes using the discriminator.
+        classes = map discr arr
+        
+        -- Here, we could assert that all the classes are between 0 and m-1.
+        
+        -- Turn the classes into columns (lists).
+        columns = map (createColumn m) classes
+        
+        -- Scan the columns.
+        scan_results = scanInc (zipWith (+)) (replicate m 0) columns
+        
+        -- The last column contains the reductions for each class, i.e., the
+        -- total number of elements belonging to each class.
+        sizes = last scan_results
+        
+        -- Since all the elements of class k come before the elements of class
+        -- k+1, we must accumulate the sizes into offsets to add to each class.
+        offsets = scanExc (+) 0 sizes
+        
+        -- Get the indices by selecting the appropriate entry from the scanned
+        -- column and adding the corresponding offset.
+        indices = zipWith (\k scan_result ->
+                           let
+                               -- Select the k'th entries.
+                               scan_result_k = scan_result !! k
+                               offset_k = offsets !! k
+                           in
+                             -- Add offset. Subtract 1 to make it 0-indexed.
+                             scan_result_k + offset_k - 1
+                          )
+                  classes scan_results
+        
+        -- Compute the resulting array based on the indices.
+        new_arr = permute indices arr
+        
+        -- Write a flag array containing the sizes (i.e., the number of elements
+        -- belonging to each class) at the positions given by the offsets.
+        new_flags = write offsets sizes (replicate n 0)
+    in
+      (new_flags, new_arr)
--- haskell_code/MdiscrSegmented.hs
+++ haskell_code/MdiscrSegmented.hs
@@ -7,96 +7,123 @@ import Helpers
 import Data.List(zipWith4)
 
 -- Segmented version.
---segmMdiscr :: Int -> (a -> Int) -> [a] -> [Int] -> ([a], [Int])
-segmMdiscr m discr segment_sizes arr = new_arr
-    where
-      -- The length of the input array.
-      n = length arr
-      
-      -- Get the segment boundaries as 1's.
-      segment_flags = map (\s -> if s /= 0 then 1 else 0) segment_sizes
-      
-      -- Sizes extended inside each segment.
-      sizes_extended = segmScanInc (+) 0 segment_flags segment_sizes
-      
-      -- Sizes accumulated across segment boundaries (in order to produce
-      -- the offsets to the segment starts).
-      sizes_accumulated = scanInc (+) 0 segment_sizes
-      
-      -- Offsets to the segment starts.
-      segment_offsets = zipWith (\size_accum size_ext -> size_accum - size_ext)
-                sizes_accumulated
-                sizes_extended
-      
-      -- Find the equivalence classes using the discriminator.
-      classes = map discr arr
-      
-      -- Here, we could assert that all the classes are between 0 and m-1.
-      
-      -- Create a list with [0,...,1,...,0] of length m where the 1 is in
-      -- the k'th position.
-      createColumn :: Int -> [Int]
-      createColumn k =
-          let
-              zeros = replicate m 0
-              (ys,_:zs) = splitAt k zeros
-          in
-            ys ++ (1 : zs)
-      
-      -- First turn the classes into columns (lists)
-      columns = map createColumn classes
-      
-      -- Scan the columns.
-      scan_results = segmScanInc (zipWith (+)) (replicate m 0) segment_flags columns
-      
-      -- Distribute the last elements of the scan_results (i.e., the reductions)
-      -- backwards across the segments.
-      reductions = zipWith (\size_ext offset -> scan_results !! (size_ext+offset-1))
-                   sizes_extended segment_offsets
-      
-      -- The reductions count the number of occurrences of each class. With the
-      -- exclusive scan, we get the offsets that we must add to each class
-      -- inside the segments.
-      class_offsets = map (scanExc (+) 0) reductions
-      
-      -- Get the indices by adding the appropriate offsets and selecting the
-      -- appropriate entries from the columns.
-      indices = zipWith4 (\k scan_result class_offset segment_offset ->
-                          -- Select the k'th entry from the column.
-                          flip (!!) k $
-                          -- Add the segment offsets to all the segments (and
-                          -- make it 0-indexed).
-                          map (+(segment_offset-1)) $
-                          -- Add the class offsets (within segments).
-                          zipWith (+) scan_result class_offset
-                         )
-                classes scan_results class_offsets segment_offsets
-      
-      -- Compute the resulting array based on the indices.
-      new_arr = permute indices arr
-      
-      -- -- Shift the flags one to the left, so that now we are accessing the last
-      -- -- elements of the segments.
-      -- shifted_flags = tail segment_flags ++ [head segment_flags]
-      
-      -- aggr_stripped = zipWith (\flag scan_result -> if flag > 0
-      --                                               then scan_result
-      --                                               -- "Neutral element" column.
-      --                                               else replicate m 0)
-      --                 shifted_flags scan_results
-      
-      
-      
-      -- -- Add the offsets.
-      -- scan_result_with_offsets = map (zipWith (+) offsets) scan_result
-      
-      -- -- Now, the indices for elements of class k can be found at the k'th entry
-      -- -- in the corresponding column of the scan result, so we must extract the
-      -- -- appropriate entries from the columns. These indices are 1-indexed, so
-      -- -- we must remember to subtract 1.
-      -- indices = zipWith (\k column -> column !! k - 1)
-      --           classes scan_result_with_offsets
-      
-      -- -- Write a flag array containing the sizes (i.e., the number of elements
-      -- -- belonging to each class) at the positions given by the offsets.
-      -- flags = write offsets sizes (replicate n 0)
+segmMdiscr :: Int -> (a -> Int) -> [Int] -> [a] -> ([Int], [a])
+segmMdiscr m discr segment_sizes arr =
+    let
+        -- The length of the input array.
+        n = length arr
+        
+        -- Get the segment boundaries as 1's.
+        segment_flags = map (\s -> if s /= 0 then 1 else 0) segment_sizes
+        
+        -- Sizes extended inside each segment.
+        sizes_extended = segmScanInc (+) 0 segment_flags segment_sizes
+        
+        -- Sizes accumulated across segment boundaries (in order to produce
+        -- the offsets to the segment starts).
+        sizes_accumulated = scanInc (+) 0 segment_sizes
+        
+        -- Offsets to the segment starts.
+        segment_offsets = zipWith (\size_accum size_ext -> size_accum - size_ext)
+                          sizes_accumulated
+                          sizes_extended
+        
+        -- Find the equivalence classes using the discriminator.
+        classes = map discr arr
+        
+        -- Here, we could assert that all the classes are between 0 and m-1.
+        
+        -- Turn the classes into columns (lists).
+        columns = map (createColumn m) classes
+        
+        -- Scan the columns.
+        scan_results = segmScanInc (zipWith (+)) (replicate m 0) segment_flags columns
+        
+        -- Distribute the last elements of the scan_results (i.e., the reductions)
+        -- backwards across the segments.
+        reductions = zipWith (\size_ext offset -> scan_results !! (size_ext+offset-1))
+                     sizes_extended segment_offsets
+        
+        -- The reductions count the number of occurrences of each class. With the
+        -- exclusive scan, we get the offsets that we must add to each class
+        -- inside the segments.
+        class_offsets = map (scanExc (+) 0) reductions
+        
+        -- Get the indices by selecting the appropriate entries from the columns
+        -- and adding the appropriate offsets.
+        indices = zipWith4 (\k scan_result class_offset segment_offset ->
+                            let
+                                -- Select the k'th entries.
+                                scan_result_k = scan_result !! k
+                                class_offset_k = class_offset !! k
+                            in
+                              -- Add offsets. Subtract 1 to make it 0-indexed.
+                              scan_result_k + class_offset_k + segment_offset - 1
+                           )
+                  classes scan_results class_offsets segment_offsets
+        
+        -- Compute the resulting array based on the indices.
+        new_arr = permute indices arr
+        
+        -- Shift the flags one to the left to make it a mask that accesses the
+        -- last elements of the segments.
+        shifted_flags = tail segment_flags ++ [head segment_flags]
+        
+        -- Mask out everyting but the last element of each segment. These
+        -- elements contain the reductions of the segments.
+        reductions_stripped = zipWith (\flag scan_result ->
+                                       if flag > 0
+                                       -- The last element of the segment
+                                       -- contains the reduction.
+                                       then scan_result
+                                       -- "Neutral element" column.
+                                       else replicate m 0)
+                              shifted_flags scan_results
+        
+        -- Loop over the class number (for k = 0..m-1). We start with a flag
+        -- array of zeros of length (n+1), and for each class k, we will write
+        -- the size of equivalence class k (in each segment) to the appropriate
+        -- place in the flag array. The 0'th element contains only "junk" and
+        -- is discarded (by taking only the tail).
+        new_flags = tail $
+                    foldl (\acc_sizes k ->
+                           let
+                               -- Here, we are producing [(index,size)], and
+                               -- then we unzip to make it ([indices],[sizes]).
+                               (indices_k, sizes_k) =
+                                   unzip $
+                                   zipWith3
+                                   -- For the current class k, extract the
+                                   -- k'th column from the masked out array
+                                   -- of reductions.
+                                   (\reduction segment_offset class_offset ->
+                                    let
+                                        reduction_k = reduction !! k
+                                        class_offset_k = class_offset !! k
+                                    in
+                                      if reduction_k > 0
+                                      -- Only if there actually were some
+                                      -- occurrences for this class do we write
+                                      -- into the flag array.
+                                      then (segment_offset + class_offset_k + 1,
+                                            reduction_k)
+                                      -- Otherwise, either there were no
+                                      -- occurrences, or we are not at a
+                                      -- reduction (i.e., we are currently at
+                                      -- an element that was masked out). Either
+                                      -- way, we simply don't write to the flag
+                                      -- array.
+                                      else (0,0) -- junk
+                                   )
+                                   reductions_stripped
+                                   segment_offsets
+                                   class_offsets
+                           in
+                             write indices_k sizes_k acc_sizes
+                          )
+                    -- We start with a flag array of zeros of length (n+1).
+                    (replicate (n+1) 0)
+                    -- Loop over the class number (for k = 0..m-1).
+                    [0..m-1]
+    in
+      (new_flags, new_arr)
